dc3cee7000e4b8bf9fe307746a0b763bfe1d9f04
Luca Panariello <luca.panariello@gmail.com>
2026-01-31 20:54:01 +0100

    aggiornato metodo deprecato

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const cluster = require('cluster');
 const config = require('./config/config');
 
-if (cluster.isMaster) {
+if (cluster.isPrimary) {
   for (let i = 0; i < config.numCPUs; i += 1) cluster.fork(); // Create a worker for each CPU
 
   cluster.on('exit', () => cluster.fork()); // Listen for dying workers and replace them
